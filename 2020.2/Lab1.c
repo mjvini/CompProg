6965dc808e48398d8f320546ec58d627e12e4b89
pedromoussa <pedropaulo.moussa@gmail.com>
2021-07-14 17:31:56 -0300

    Added some labs

---
+++ 2020.2/Lab1.c
@@ -0,0 +1,437 @@
+/*
+ * Primeiro laboratório de Computadores e Programação
+ *
+ * A ideia do laboratório é mostrar ao aluno outras formas de fazer as operações
+ * que ele já está acostumado a fazer, mas de formas diferentes.
+ * Criamos esse desafio com o objetivo de fazê-lo pensar em diferentes formas de se
+ * atingir o mesmo resultado. Se possível, tentando pensar em qual seria mais
+ * eficiente.
+ *
+ * O aluno deverá usar apenas um subset de operações de C para realizar o que
+ * for pedido no enunciado.
+ *
+ * Todas as operações permitidas serão especificadas em cada questão.
+ *
+ * Forma de avaliação:
+ *      - Quantas operações o aluno utlizou para realizar a tarefa (dentro da quantidade aceitável)
+ *      - Explicação do código -- deverá ser o mais claro possível (como qualquer
+ *          código), imaginando que qualquer pessoa sem conhecimento prévio da
+ *          matéria consiga entender o que foi feito.
+ *      - As resoluções com menos operações do que a do monitor terão bonificação.
+ *
+ * Assinatura:
+ *      Aluno: <nome> Rodrigo Côrtes Nogueira da Rocha Pita
+ *      DRE: <DRE> 118187443
+ *      Aluno: <nome> Pedro Paulo Moussa
+ *      DRE: <DRE> 119181709
+ *      versão do GCC utilizada: 9.3.0
+ *
+ */
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+/* Número é par ou não
+ *      Permitido:
+ *          Operações: ~ & ^ | << >>
+ *
+ *      Número máximo de operações: 3
+ *      Monitor: 2
+ *
+ *      Retorna 1 se x é par, retorna 0 caso contrário
+ *
+ *      Exemplo:
+ *          ehPar(0) -> 1
+ *          ehPar(2) -> 1
+ *          ehPar(7) -> 0
+ */
+int32_t ehPar(int32_t x) {
+    return ((x & 1) == 0); //se x&1 der 0, significa que x acaba em 0, ou seja, o número é divisível por 2
+}
+
+/*
+ * Módulo 8
+ *      Permitido:
+ *          Operações: ~ & ^ | ! << >>
+ *
+ *      Número máximo de operações: 3
+ *      Monitor: 1
+ *
+ *      Retorna x % 8
+ *
+ *      Exemplo:
+ *          mod8(1) -> 1
+ *          mod8(7) -> 7
+ *          mod8(10) -> 2
+ */
+int32_t mod8(int32_t x) {
+    x = x & 7; //usamos x & 7 , pois mod8 é um grupo abeliano de 0 à 7, para saber qual termo do grupo um número é, tiramos o resíduo de uma divisão por 8
+    return x;
+}
+
+/* Negativo sem -
+ *      Permitido:
+ *          Operações: ~ & ^ | ! << >> +
+ *
+ *      Número máximo de operações: 5
+ *      Monitor: 2
+ *
+ *      Retorna -x
+ *
+ *      Exemplo:
+ *          negativo(1) -> -1
+ *          negativo(42) -> -42
+ */
+int32_t negativo(int32_t x) {
+    x = ~x + 1;
+    return x; //para transformar o número em negativo sem '-', usamos o complemento a dois do número, que é representado por inverter os 1s e 0s e somar 1
+}
+
+/* Implementação do & usando bitwise
+ *      Permitido:
+ *          Operações: ~ ^ | ! << >>
+ *
+ *      Número máximo de operações: 7
+ *      Monitor: 4
+ *
+ *      Retorna x & y
+ *
+ *      Exemplo:
+ *          bitwiseAnd(1, 2) -> 0
+ *              01 & 10 -> 00
+ *          bitwiseAnd(3, 11) -> 3
+ *              11 & 1011 -> 0011
+ */
+int32_t bitwiseAnd(int32_t x, int32_t y) {
+    return (~(~x | ~y)); //Pegamos a negação de x e ye tiramos o OU entre eles e depois negamos o resultado de novo. Essa segunda parte é para cuidar dos casos em que os dois números são iguais.
+    //Então se 
+    //x = 0 e y = 0, pegamos 1 OU 1 = 1 e negamos, dando 0
+    //x = 1 e y = 0, pegamos 0 OU 1 = 1 e negamos, dando 0
+    //x = 0 e y = 1, pegamos 1 OU 0 = 1 e negamos, dando 0
+    //x = 1 e y = 1, pegamos 0 OU 0 = 0 e negamos, dando 1
+}
+
+/* Igual sem ==
+ *      Permitido:
+ *          Operações: ~ & ^ | << >> !
+ *
+ *      Número máximo de operações: 3
+ *      Monitor: 2
+ *
+ *      Retorna 1 se x == y, 0 caso contrário
+ *
+ *      Exemplo:
+ *          ehIgual(10, 10) -> 1
+ *          ehIgual(16, 8) -> 0
+ */
+int32_t ehIgual(int32_t x, int32_t y) {
+    return (!(x ^ y)); //o XOR (OU Exclusivo) retorna 1 quando os valores comparados forem diferentes e 0, se forem iguais,
+    // então a negação do XOR retornará 1 para valores iguais e 0 para diferentes
+}
+
+/* Limpa bit n
+ *      Permitido:
+ *          Operações: ~ & ^ | ! << >>
+ *
+ *      Número máximo de operações: 4
+ *      Monitor: 3
+ *
+ *      Retorna o x com o bit n = 0,
+ *      n pode variar entre 0 e 31, do LSB ao MSB
+ *
+ *      Exemplo:
+ *          limpaBitN(3, 0) -> 2
+ *          limpaBitN(3, 1) -> 1
+ */
+int32_t limpaBitN(int32_t x, int8_t n) {
+    int32_t y = ~(1 << n); //y = 0000 0000 0000 0001 , coloca o bit 1 na posicao n e depois inverte y
+    return x & y;
+}
+
+/*
+ * Bit na posição p do inteiro x
+ *      Permitido:
+ *          Operações bitwise: << >> | & + -
+ *
+ *      Número máximo de operações: 6
+ *      Monitor: 2
+ *
+ *      Retorna o valor do bit na posição p no inteiro x
+ *      Valor de retorno pode ser apenas 0 ou 1
+ *
+ *      p será um valor entre 0 e 31
+ *
+ *      Exemplo:
+ *          23 em binário: 0   0 ... 0 1 0 1 1 1
+ *               posições: 31 30 ... 5 4 3 2 1 0
+ *
+ *          bitEmP(23, 31) -> 0
+ *          bitEmP(23, 5) -> 0
+ *          bitEmP(23, 4) -> 1
+ *          bitEmP(23, 3) -> 0
+ *          bitEmP(23, 2) -> 1
+ *          bitEmP(23, 1) -> 1
+ *          bitEmP(23, 0) -> 1
+ *
+ */
+int32_t bitEmP(int32_t x, uint8_t p) {
+    return ((x >> p) & 1);
+    //bitshift e depois verificar se é par
+}
+
+/*
+ * Byte na posição p do inteiro x
+ *      Permitido:
+ *          Operações: << >> | ! &
+ *
+ *      Número máximo de operações: 6
+ *      Monitor: 3
+ *
+ *      Retorna o valor do bit na posição p no inteiro x
+ *      Valor de retorno pode ser entre 0 e 0xFF
+ *
+ *      p será um valor entre 0 e 3
+ *      0 retorna LSB
+ *      3 retorna MSB
+ *
+ *      Exemplo:
+ *          byteEmP(0x12345678, 0) -> 0x78
+ *          byteEmP(0x12345678, 1) -> 0x56
+ *          byteEmP(0x12345678, 2) -> 0x34
+ *          byteEmP(0x12345678, 3) -> 0x12
+ *
+ */
+int32_t byteEmP(int32_t x, uint8_t p) {
+    x = x << 24-(8*p);
+    x = x >> 24;
+    return x;
+    //a função faz um left shift até chegar no byte desejado e os bits shiftados pra fora são perdidos,
+    //depois um right shift até que o byte desejado seja o lsb
+    //como os bits são perdidos, o right shift coloca tudo como 0
+    //0x12345678 = 0001 0010 0011 0100 0101 0110 0111 1000
+    //p = 1
+    //x << 24 - (8*p) = 0101 0110 0111 1000
+    //x >> 24 = 0000 0000 0000 0000 0000 0000 0101 0110 = 0x56
+    //p = 2
+    //x << 24 - (8*p) = 0011 0100 0101 0110 0111 1000
+    //x >> 24 = 0000 0000 0000 0000 0000 0000 0011 0100 = 0x34
+    //p = 3
+    //x << 24 - (8*p) = 0001 0010 0011 0100 0101 0110 0111 1000
+    //x >> 24 = 0000 0000 0000 0000 0000 0000 0001 0010 = 0x12
+}
+
+/*
+ * Seta byte na posição p do inteiro x como y
+ *      Permitido:
+ *          Operações: << >> | ~ ! &
+ *
+ *      Número máximo de operações: 7
+ *      Monitor: 5
+ *
+ *      Retorna x com o valor y no byte da posição p
+ *
+ *      p será um valor entre 0 e 3
+ *      0 retorna LSB
+ *      3 retorna MSB
+ *
+ *      Exemplo:
+ *          setaByteEmP(0x12345678, 0xFF, 0) -> 0x123456FF
+ *          setaByteEmP(0x12345678, 0xFF, 1) -> 0x1234FF78
+ *          setaByteEmP(0x12345678, 0xFF, 2) -> 0x12FF5678
+ *          setaByteEmP(0x12345678, 0xFF, 3) -> 0xFF345678
+ *
+ */
+int32_t setaByteEmP(int32_t x, int32_t y, uint8_t p) {
+    return x |= y << 8 * p;
+    //0x12345678 = 0001 0010 0011 0100 0101 0110 0111 1000
+    //0xFF = 0000 0000 0000 0000 0000 0000 0000 1111 1111
+}
+
+/*
+ * Minimo
+ *      Permitido:
+ *          Operações: << >> | ^ < > ~ ! & -
+ *
+ *      Número máximo de operações: 15
+ *      Monitor: 5
+ *
+ *      Retorna o menor numero entre x e y
+ *
+ *      Exemplo:
+ *          minimo(10, 15) -> 10
+ *          minimo(-2, -1) -> -2
+ *          minimo(-1, 2) -> -1
+ *
+ */
+int32_t minimo(int32_t x, int32_t y) {
+    return y ^ ((x ^ y) & - (x < y)); //funciona, pois se x>y, então -(x<y) = -1 (11111...), então ficará y^((x^y) & (111111...) que fica y^x^y = x
+    //caso contrário, -(x<y) = 0, logo a função retorna y^((x^y) & 0) = y^0 = y
+}
+
+/*
+ * Negação lógica sem !
+ *      Permitido:
+ *          Operações: << >> | & + ~
+ *
+ *      Número máximo de operações: 15
+ *      Monitor: 5
+ *
+ *      Retorna 1 se x == 0, retorna 0 caso contrário
+ *
+ *      Exemplo:
+ *          negacaoLogica(0) -> 1
+ *          negacaoLogica(37) -> 0
+ *
+ */
+int32_t negacaoLogica(int32_t x) {
+  return ((x >> 31) | ((~x + 1) >> 31)) + 1; //shiftamos tanto x, quanto a negacao de x 31 casas para a direita.
+}
+
+void teste(int32_t saida, int32_t esperado) {
+    static uint8_t test_number = 0;
+    test_number++;
+    if(saida == esperado)
+        printf(ANSI_COLOR_GREEN "PASSOU! Saída: %-10d\t Esperado: %-10d\n" ANSI_COLOR_RESET,
+            saida, esperado);
+
+    else
+        printf(ANSI_COLOR_RED "%d: FALHOU! Saída: %-10d\t Esperado: %-10d\n" ANSI_COLOR_RESET,
+            test_number, saida, esperado);
+}
+
+int main() {
+    puts(ANSI_COLOR_BLUE "Primeiro lab - bits" ANSI_COLOR_RESET);
+    puts("");
+
+    puts("Teste: ehPar");
+    teste(ehPar(2), 1);
+    teste(ehPar(1), 0);
+    teste(ehPar(3), 0);
+    teste(ehPar(13), 0);
+    teste(ehPar(100), 1);
+    teste(ehPar(125), 0);
+    teste(ehPar(1024), 1);
+    teste(ehPar(2019), 0);
+    teste(ehPar(2020), 1);
+    teste(ehPar(-1), 0);
+    teste(ehPar(-27), 0);
+    teste(ehPar(-1073741825), 0);
+    teste(ehPar(1073741824), 1);
+    teste(ehPar(2147483647), 0);
+    teste(ehPar(-2147483648), 1);
+    teste(ehPar(0), 1);
+    puts("");
+
+    puts("Teste: mod8");
+    teste(mod8(0), 0);
+    teste(mod8(4), 4);
+    teste(mod8(7), 7);
+    teste(mod8(8), 0);
+    teste(mod8(-1), 7);
+    teste(mod8(-8), 0);
+    teste(mod8(2147483647), 7);
+    teste(mod8(-2147483648), 0);
+    puts("");
+
+    puts("Teste: negativo");
+    teste(negativo(0), 0);
+    teste(negativo(1), -1);
+    teste(negativo(-1), 1);
+    teste(negativo(2147483647), -2147483647);
+    teste(negativo(-2147483647), 2147483647);
+    teste(negativo(-2147483648), 2147483648);
+    puts("");
+
+    puts("Teste: bitwiseAnd");
+    teste(bitwiseAnd(1, 3), 1);
+    teste(bitwiseAnd(-1, 0), 0);
+    teste(bitwiseAnd(-1, 0x7FFFFFFF), 0x7FFFFFFF);
+    teste(bitwiseAnd(0b0100, 0b1100), 0b0100);
+    puts("");
+
+    puts("Teste: ehIgual");
+    teste(ehIgual(1,1), 1);
+    teste(ehIgual(1,0), 0);
+    teste(ehIgual(0,1), 0);
+    teste(ehIgual(-1,1), 0);
+    teste(ehIgual(-1,-1), 1);
+    teste(ehIgual(2147483647,-1), 0);
+    teste(ehIgual(2147483647,-2147483647), 0);
+    teste(ehIgual(2147483647,-2147483648), 0);
+    teste(ehIgual(2147483647,-2147483648), 0);
+    puts("");
+
+    puts("Teste: limpaBitN");
+    teste(limpaBitN(1,0), 0);
+    teste(limpaBitN(0b1111,1), 0b1101);
+    teste(limpaBitN(15,3), 7);
+    teste(limpaBitN(-1,31), 2147483647);
+    teste(limpaBitN(-1,0), -2);
+    teste(limpaBitN(2147483647, 30), 1073741823);
+    puts("");
+
+    puts("Teste: bitEmP");
+    teste(bitEmP(1, 0), 1);   //    b01 => retorna 1
+    teste(bitEmP(1, 1), 0);   //    b01 => retorna 0
+    teste(bitEmP(2, 0), 0);   //    b10 => retorna 0
+    teste(bitEmP(2, 1), 1);   //    b10 => retorna 1
+    teste(bitEmP(9, 2), 0);   //  b1001 => retorna 0
+    teste(bitEmP(-4194305, 22), 0);
+    teste(bitEmP(9, 3), 1);
+    teste(bitEmP(16, 3), 0);
+    teste(bitEmP(0x1 << 5, 4), 0);
+    teste(bitEmP(0x1 << 31, 31), 1);
+    teste(bitEmP(-1073741825, 30), 0);
+    teste(bitEmP(-1073741825, 31), 1);
+    puts("");
+
+    puts("Teste: byteEmP");
+    teste(byteEmP(0x766B, 1), 0x76);
+    teste(byteEmP(0x766B, 0), 0x6B);
+    teste(byteEmP(0x8420, 0), 0x20);
+    teste(byteEmP(0x12345678, 3), 0x12);   // retorna 0x12
+    teste(byteEmP(0x12345678, 2), 0x34);   // retorna 0x34
+    teste(byteEmP(0x12345678, 1), 0x56);   // retorna 0x56
+    teste(byteEmP(0x12345678, 0), 0x78);   // retorna 0x78
+    teste(byteEmP(0x321, 1), 0x03);        // retorna 0x03
+    teste(byteEmP(0x321, 0), 0x21);        // retorna 0x21
+    puts("");
+
+    puts("Teste: setaByteEmP");
+    teste(setaByteEmP(0x00, 0xFF, 0), 0x000000FF);
+    teste(setaByteEmP(0x00, 0xFF, 1), 0x0000FF00);
+    teste(setaByteEmP(0x00, 0xFF, 2), 0x00FF0000);
+    teste(setaByteEmP(0x00, 0xFF, 3), 0xFF000000);
+    teste(setaByteEmP(0x01234567, 0x33, 2), 0x01334567);
+    teste(setaByteEmP(0xdeadbeef, 0x00, 0), 0xdeadbe00);
+    teste(setaByteEmP(0xdeadbeef, 0x00, 1), 0xdead00ef);
+    puts("");
+
+    puts("Teste: minimo");
+    teste(minimo(0,1), 0);
+    teste(minimo(0,10), 0);
+    teste(minimo(1, 128), 1);
+    teste(minimo(-1, 0), -1);
+    teste(minimo(-1, -2), -2);
+    teste(minimo(2147483647, 2147483646), 2147483646);
+    teste(minimo(-2147483648, -2147483647), -2147483648);
+    teste(minimo(-2147483648, -1), -2147483648);
+    puts("");
+
+    puts("Teste: negacaoLogica");
+    teste(negacaoLogica(0), 1);
+    teste(negacaoLogica(1), 0);
+    teste(negacaoLogica(-1), 0);
+    teste(negacaoLogica(64), 0);
+    teste(negacaoLogica(-64), 0);
+    teste(negacaoLogica(2147483647), 0);
+    teste(negacaoLogica(-2147483648), 0);
+    puts("");
+
+}
